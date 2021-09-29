a4249ec123df0e3b0014a3a1b5f5e9ff2cd44b38
Abid Rahman <abid@opn.ooo>
2021-09-29 17:28:02 +0600

    0.10

--- Package.swift
+++ Package.swift
@@ -16,13 +16,13 @@ let package = Package(
             targets: ["FrameworkATarget"]),
     ],
     dependencies: [
-        .package(name: "FrameworkB", url: "git@github.com:abid-opn/FrameworkB.git", from: "0.1.0")
+        .package(name: "FrameworkBWrapper", url: "git@github.com:abid-opn/FrameworkBWrapper.git", from: "0.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(name: "FrameworkADependencies", dependencies: [
-            "FrameworkB"
+            "FrameworkBWrapper"
         ]),
         .binaryTarget(name: "FrameworkA", path: "artifacts/FrameworkA.xcframework"),
         .target(name: "FrameworkATarget", dependencies: [
