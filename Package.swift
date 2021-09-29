029a55f1beadfec461eab89cefdcb7f6f78fdd79
Abid Rahman <abid@opn.ooo>
2021-09-29 11:22:20 +0600

    updt

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "FrameworkAWrapper",
-            targets: ["FrameworkA", "FrameworkADependencies"]),
+            targets: ["FrameworkATarget"]),
     ],
     dependencies: [
         .package(name: "FrameworkB", url: "git@github.com:abid-opn/FrameworkB.git", from: "0.1.0")
@@ -21,7 +21,13 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(name: "FrameworkADependencies", dependencies: ["FrameworkB"]),
-        .binaryTarget(name: "FrameworkA", path: "artifacts/FrameworkA.xcframework")
+        .target(name: "FrameworkADependencies", dependencies: [
+            "FrameworkB"
+        ]),
+        .binaryTarget(name: "FrameworkA", path: "artifacts/FrameworkA.xcframework"),
+        .target(name: "FrameworkATarget", dependencies: [
+            .target(name: "FrameworkA", condition: .when(platforms: [.iOS])),
+            .target(name: "FrameworkADependencies", condition: .when(platforms: [.iOS]))
+        ])
     ]
 )
---
+++ Sources/FrameworkATarget/Dummy.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Abid Rahman on 28/9/21.
+//
+
+import Foundation
